c3c67663845c8e9b46a9a4c51402621045ae230d
Shivam Patanwar <shivampatanwarofficial@gmail.com>
2024-09-24 01:21:10 +0530

    s

---
+++ src/TASK/ThemeContextAPI/ThemeContextAPI.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Container1 from './Container1';
+import Container2 from './Container2';
+import Container3 from './Container3';
+import Container4 from './Container4';
+import ThemeChanger from './ThemeChanger';
+import './ThemeContextAPI.css'
+
+
+const ThemeContextAPI = () => {
+  return (
+    <div className="task">
+        <h1 className='task-header'>ThemeContextAPI</h1>
+        <div className="task-container ThemeContextAPI" >
+          <ThemeChanger>
+            <Container1 />
+            <Container2 />
+            <Container3 />
+            <Container4 />
+          </ThemeChanger>
+        </div>
+      </div>
+  )
+}
+
+export default ThemeContextAPI
---
+++ src/TASK/Youtube/Youtube.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import VideoPlayer from "./VideoPlayer";
+import JSON from "./video.json";
+import Nav from "./Nav";
+import "./Youtube.css";
+
+const Youtube = () => {
+  let [data, setData] = useState(JSON);
+
+  let [play, setPlay] = useState(data[0]);
+
+  let handlePlay = (datas) => {
+    setPlay(datas);
+  };
+
+  return (
+    <div className="task">
+      <h1 className="task-header">YouTube</h1>
+      <div className="task-container Youtube">
+        <Nav />
+        <VideoPlayer data={data} play={play} handlePlay={handlePlay} />
+      </div>
+    </div>
+  );
+};
+
+export default Youtube;
