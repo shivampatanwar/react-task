c050b5e029c1b5ff4a9dd054471dfc4601087194
Shivam Patanwar <shivampatanwarofficial@gmail.com>
2024-09-25 01:53:41 +0530

    s

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"react-task",
+  base:"/react-task/",
   server: {
     host: true,
     port: 8080,
     open: true,
+    https: true,
   }
 })
